4c8894aca940a0c5ada3acf4939bddda649326c0
Miguel de Alba <miguelalba89@gmail.com>
2021-05-07 10:30:07 +0200

    Add basic frontend and file upload method

--- resources/templates/index.ftl
+++ resources/templates/index.ftl
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Hello world</title>
+    <title>FSKX Validator</title>
 </head>
 <body>
-<p>Example page</p>
+    <h1>FSKX Validator</h1>
+    <p><input id="fileInput" type="file"> <button id="validateButton">Validate</button></p>
+    <div>
+        <p>Results</p>
+        <div></div>
+    </div>
+    <script>
+        document.getElementById("validateButton").onclick = () => {
+            let file = document.getElementById("fileInput").files[0];
+            let data = new FormData();
+            data.append('file', file);
+            data.append('user', 'webapp');
+            fetch("/validate", { method: 'POST', body: data });
+        }
+    </script>
 </body>
 </html>
--- src/Application.kt
+++ src/Application.kt
@@ -8,10 +8,17 @@ import io.ktor.freemarker.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.jackson.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.io.File
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createTempDirectory
 
+@OptIn(ExperimentalPathApi::class)
 fun Application.module(testing: Boolean = false) {
+
+    val TEMP_FOLDER = createTempDirectory("uploads")
 
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
@@ -35,6 +42,40 @@ fun Application.module(testing: Boolean = false) {
         val viewData = object {} // TODO: fill viewData
         get("/") {
             call.respond(FreeMarkerContent("index.ftl", mapOf("viewData" to viewData), ""))
+        }
+
+        /*
+         * Body parameter: form data:
+         * - file: Binary object
+         * - user: User name (string)
+         */
+        post("/validate") {
+            val multipartData = call.receiveMultipart()
+
+            var file: File? = null
+            multipartData.forEachPart { part ->
+                // if part is a file (could be form item)
+                if(part is PartData.FileItem) {
+                    // retrieve file name of upload
+                    val name = part.originalFileName!!
+
+                    val fileCopy = kotlin.io.path.createTempFile(TEMP_FOLDER, name).toFile()
+                    fileCopy.deleteOnExit()
+
+                    val fileBytes = part.streamProvider().readBytes()
+                    fileCopy.writeBytes(fileBytes)
+
+                    file = fileCopy
+                }
+                // make sure to dispose of the part after use to prevent leaks
+                part.dispose()
+            }
+
+            if (file != null) {
+                // TODO: validate file
+                call.respond(HttpStatusCode.OK)
+            }
+            println("TODO: /validate")
         }
 
         static("/static") {
