289e6a1b7af431677a529726e2d9cce04f28e96c
Miguel de Alba <miguelalba89@gmail.com>
2021-05-21 21:07:47 +0200

    Add checks for broken scripts

--- test/CodeCheckerTest.kt
+++ test/CodeCheckerTest.kt
@@ -13,4 +13,22 @@ class CodeCheckerTest {
         assertTrue(checkResult.error.isEmpty())
         assertTrue(checkResult.warnings.isEmpty())
     }
+
+    @Test
+    fun testBrokenModel() {
+        val file = File("testresources/broken_model.fskx")
+        val checkResult = CodeChecker().check(file)
+
+        assertTrue(checkResult.error.isNotEmpty())
+        assertTrue(checkResult.warnings.isEmpty())
+    }
+
+    @Test
+    fun testBrokenVisualization() {
+        val file = File("testresources/broken_visualization.fskx")
+        val checkResult = CodeChecker().check(file)
+
+        assertTrue(checkResult.error.isNotEmpty())
+        assertTrue(checkResult.warnings.isEmpty())
+    }
 }
